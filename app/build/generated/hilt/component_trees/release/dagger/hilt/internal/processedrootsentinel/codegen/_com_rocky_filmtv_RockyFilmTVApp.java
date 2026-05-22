b7c1515beabe9891f2eb510c9cac17c265766d3a
Ngoc Thach <ngocthachtruongnguyen@gmail.com>
2026-05-22 15:34:39 +0700

    chore: remove build files from git index

--- app/build/generated/hilt/component_trees/release/dagger/hilt/internal/processedrootsentinel/codegen/_com_rocky_filmtv_RockyFilmTVApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.rocky.filmtv.RockyFilmTVApp"
-)
-public final class _com_rocky_filmtv_RockyFilmTVApp {
-}
